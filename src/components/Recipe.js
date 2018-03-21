9d32128e524f0d5ae5ea5682eec913caff6ec55a
Caleb Lee <caleb.gwl@gmail.com>
2018-03-21 16:10:45 -0500

    Render rest of properties

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 class Recipe extends Component {
   render() {
+    const recipe = this.props;
     return (
       <div className="recipe">
-        <h1>Recipe</h1>
+        <img src={recipe.image} alt={recipe.name}/>
+        <p>{recipe.name}</p>
+        <p>{recipe.image}</p>
+        <p>{recipe.ingredients}</p>
+        <p>{recipe.directions}</p>
+        <p>{recipe.cookTime}</p>
+        <p>{recipe.servings}</p>
       </div>
     );
   }
