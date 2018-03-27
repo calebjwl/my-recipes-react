c18990b6f1c1b8a369c1b1bd9f398f09c8552a51
Caleb Lee <caleb.gwl@gmail.com>
2018-03-27 12:47:00 -0500

    AddRecipe component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import RecipeGrid from './RecipeGrid';
 import Recipe from './Recipe';
+import AddRecipe from './AddRecipe';
 
 const Main = () => (
   <Switch>
     <Route exact path='/' component={RecipeGrid}/>
     <Route path='/recipe/:code' component={Recipe}/>
+    <Route path='/add-recipe' component={AddRecipe}/>
   </Switch>
 );
 
