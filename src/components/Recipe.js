e67ef9e7c58749851a3b96ea17cc0d81fa27f45e
Caleb Lee <caleb.gwl@gmail.com>
2018-03-27 12:17:10 -0500

    Convert recipe to react component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,23 +3,30 @@ import { Link } from 'react-router-dom';
 
 import RecipeAPI from '../api';
 
-const Recipe = props => {
-  const recipe = RecipeAPI.get(parseInt(props.match.params.id, 10));
-  if (!recipe) {
-    return <div>Sorry, but the recipe was not found.</div>;
+class Recipe extends Component {
+  constructor(props) {
+    super();
+    this.props = props;
   }
-  return (
-    <div className="recipe">
-      <Link to={`/recipe/${recipe.id}`}>
-        <img src={recipe.image} alt={recipe.name}/>
-      </Link>
-      <p>{recipe.name}</p>
-      <p>{recipe.ingredients}</p>
-      <p>{recipe.directions}</p>
-      <p>Cook time: {recipe.cookTime}</p>
-      <p>Servings: {recipe.servings}</p>
-    </div>
-  );
+
+  render() {
+    const recipe = RecipeAPI.get(parseInt(this.props.match.params.id, 10));
+    if (!recipe) {
+      return <div>Sorry, but the recipe was not found.</div>;
+    }
+    return (
+      <div className="recipe">
+        <Link to={`/recipe/${recipe.id}`}>
+          <img src={recipe.image} alt={recipe.name}/>
+        </Link>
+        <p>{recipe.name}</p>
+        <p>{recipe.ingredients}</p>
+        <p>{recipe.directions}</p>
+        <p>Cook time: {recipe.cookTime}</p>
+        <p>Servings: {recipe.servings}</p>
+      </div>
+    );
+  }
 }
 
 export default Recipe;
