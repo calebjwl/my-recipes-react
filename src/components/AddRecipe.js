871fba10112205d9f10f6bbec534e7144ec9238a
Caleb Lee <caleb.gwl@gmail.com>
2018-03-28 11:31:11 -0500

    Mockup add recipe form

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
+class AddRecipe extends Component {
   render() {
     return (
       <div>
         <h1>Add Recipe</h1>
+        <form action="">
+          <div className="control">
+            <label className="label">Recipe Name</label>
+            <input type="text"/>
+          </div>
+          <div className="control">
+            <label className="label">Image Link</label>
+            <input type="text"/>
+          </div>
+          <div className="control">
+            <label className="label">Ingredients</label>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+          <div className="control">
+            <label className="label">Directions</label>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+          <div className="control">
+            <label className="label">Cook Time</label>
+            <input type="text"/>
+          </div>
+          <div className="control">
+            <label className="label">Numer of Servings</label>
+            <input type="text"/>
+          </div>
+          <input type="submit" className="submit" value="submit"/>
+        </form>
       </div>
     );
   }
 }
 
-export default Header;
+export default AddRecipe;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import '../style/App.scss';
 
 import RecipeGrid from './RecipeGrid';
 import Recipe from './Recipe';
