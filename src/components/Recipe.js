a200054f0afd9acdf75561117673603bf8dfe8a0
Caleb Lee <caleb.gwl@gmail.com>
2018-04-02 14:11:53 -0500

    Combine recipe form and addrecipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
