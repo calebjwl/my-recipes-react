4fbce39d305bcc3508f20279159ab2fe2c171b22
Caleb Lee <caleb.gwl@gmail.com>
2018-04-02 14:14:22 -0500

    Combine app and main

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,25 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Main from './Main';
+
+import '../style/App.scss';
+import RecipeGrid from './RecipeGrid';
+import Recipe from './Recipe';
+import AddRecipe from './AddRecipe';
+
+class Main extends Component {
+  render() {
+    return (
+      <Switch>
+        <div className="container">
+          <Route exact path='/' component={RecipeGrid} />
+          <Route path='/recipe/:code' component={Recipe} />
+          <Route path='/add-recipe' component={AddRecipe} />
+        </div>
+      </Switch>
+    )
+  }
+};
 
 function mapStateToProps(state) {
   return {
