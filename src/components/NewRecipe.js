a37a1db955ff478f8f1b99b8e42a6b59649ef7cd
Caleb Lee <caleb.gwl@gmail.com>
2018-04-10 14:09:35 -0500

    Implement basic react-form for submitting data to component state

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Form, Text } from 'react-form';
 
 class NewRecipe extends Component {
-
   constructor(props) {
     super(props);
     this.state = {};
@@ -10,31 +9,63 @@ class NewRecipe extends Component {
 
   render() {
     return (
-      <div>
-        <Form
-          onSubmit={submittedValues => this.setState({ submittedValues })}>
+      <div className="add-recipe">
+        <div className="add-recipe__header">
+          <h1>Add Recipe</h1>
+          {/* <button onClick={this.loadSampleRecipe} className="button delete" type="button">Load a Random Recipe</button> */}
+        </div>
+        <Form onSubmit={recipe => this.setState({ recipe })}>
           {formApi => (
-            <div>
-              <button
-                onClick={() => formApi.addValue('siblings', '')}
-                type="button"
-                className="mb-4 mr-4 btn btn-success">Add Sibling</button>
-              <form onSubmit={formApi.submitForm} id="dynamic-form">
-                <li><Text field="ingredient-1"/></li>
-                <li><Text field="ingredient-2"/></li>
-                <li><Text field="ingredient-3"/></li>
-                {formApi.values.siblings && formApi.values.siblings.map((sibling, i) => (
-                  <li key={`sibling${i}`}>
-                    <Text field={['siblings', i]} id={`sibling-name-${i}`} />
-                    <button
-                      onClick={() => formApi.removeValue('siblings', i)}
-                      type="button"
-                      className="mb-4 btn btn-danger">Remove</button>
+            <form onSubmit={formApi.submitForm} className="form">
+              <div className="control">
+                <label className="control__label">Recipe Name</label>
+                <Text field="name" />
+              </div>
+              <div className="control">
+                <label className="control__label">Image Link</label>
+                <Text field="image" />
+              </div>
+              <div className="control">
+                <label className="control__label">Ingredients:</label>
+                <ul className="" ref="ingredients">
+                  <li>
+                    <Text field={['ingredients', 0]}/>
                   </li>
-                ))}
-                <button type="submit" className="mb-4 btn btn-primary">Submit</button>
-              </form>
-            </div>
+                  <li>
+                    <Text field={['ingredients', 1]}/>
+                  </li>
+                </ul>
+                <ul>
+                  <li className="add-item submit" onClick={this.addIngredient}>+</li>
+                </ul>
+              </div>
+              <div className="control">
+                <label className="control__label">Directions:</label>
+                <ul className="recipe__directions" ref="directions">
+                  <li>
+                    <Text field={['directions', 0]} />
+                  </li>
+                  <li>
+                    <Text field={['directions', 1]} />
+                  </li>
+                </ul>
+                <ul>
+                  <li className="add-item submit" onClick={this.addDirection}>+</li>
+                </ul>
+              </div>
+              <div className="control">
+                <label className="control__label">Cook Time (minutes)</label>
+                <Text field="cookTime" type="number"/>
+              </div>
+              <div className="control">
+                <label className="control__label">Number of Servings</label>
+                <Text field="servings" type="number"/>
+              </div>
+              <div className="right">
+                {/* <button onClick={this.clearForm} className="button warning" type="button">Clear</button> */}
+                <button type="submit" className="button submit">Submit</button>
+              </div>
+            </form>
           )}
         </Form>
       </div>
